153fa1a9de976c9e65e193a713f7ac0a5cb28497
Vivien <34387011+vivien8261@users.noreply.github.com>
2022-10-26 17:54:33 +0800

    update

--- src/weibo/helper.py
+++ src/weibo/helper.py
@@ -39,16 +39,11 @@ class WeiboContent:
 class WeiboUser:
     def __init__(self, weibo_id: int):
         self.headers = {
+            'User-Agent': ua.random if ua else 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
             'Content-Type': 'application/json; charset=utf-8',
             'Referer': f'https://m.weibo.cn/u/{weibo_id}',
             'Accept-Language': 'zh-CN,zh;q=0.9'
         }
-        if ua:
-            self.headers = {
-                **self.headers,
-                'User-Agent': ua.random
-            }
-
         self.url = 'https://m.weibo.cn/api/container/getIndex'
         self.weibo_id = weibo_id
         self.user_name = ''
