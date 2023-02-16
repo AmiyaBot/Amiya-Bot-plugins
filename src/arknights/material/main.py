42a473fb0bf1ab3e64a1b7d19334115530fffdf3
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-02-16 17:14:22 +0800

    Update main.py

--- src/arknights/material/main.py
+++ src/arknights/material/main.py
@@ -144,7 +144,7 @@ class MaterialPluginInstance(PluginInstance):
 
 bot = MaterialPluginInstance(
     name='明日方舟材料物品查询',
-    version='1.4',
+    version='1.5',
     plugin_id='amiyabot-arknights-material',
     plugin_type='official',
     description='查询明日方舟材料和物品资料',
