d36785c2bff6a222566aa2820fd60da699a54d3e
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-06-08 16:41:02 +0800

    fix 修复敌人数据解析问题

--- src/arknights/enemy/main.py
+++ src/arknights/enemy/main.py
@@ -75,7 +75,7 @@ class Enemy:
 
 bot = PluginInstance(
     name='明日方舟敌方单位查询',
-    version='2.0',
+    version='2.1',
     plugin_id='amiyabot-arknights-enemy',
     plugin_type='official',
     description='查询明日方舟敌方单位资料',
