477e8b035a1bab756c32440d640a0aa94a2ca5d1
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-08-14 14:58:44 +0800

    update 更新森空岛

--- src/skland/main.py
+++ src/skland/main.py
@@ -46,7 +46,7 @@ class SKLandPluginInstance(AmiyaBotPluginInstance):
 
 bot = SKLandPluginInstance(
     name='森空岛（Beta）',
-    version='2.0',
+    version='2.1',
     plugin_id='amiyabot-skland',
     plugin_type='official',
     description='通过森空岛 API 查询玩家信息展示游戏数据',
