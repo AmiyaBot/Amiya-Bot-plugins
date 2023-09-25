742b471ec88d1132cd230f67bc4485df6dc1f435
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-09-25 18:13:35 +0800

    update 计算器升级

---
+++ src/arknights/calculator/jade.py
@@ -0,0 +1,91 @@
+import time
+
+from amiyabot import Chain
+from core.util import extract_time
+
+sign_in = 200
+daily_tasks = 100
+weekly_tasks = 500
+weekly_battle = 1800
+
+
+async def calc_jade(reply: Chain, text: str):
+    try:
+        time_array = extract_time(text)
+        if time_array:
+            time_array = time_array[-1]
+
+            year = time.localtime().tm_year
+
+            if time_array.tm_year - year > 100:
+                return reply.text(
+                    '博士，这片大地变幻莫测，罗德岛的未来有太多不可预期，阿米娅觉得我们更应该把眼光放在当下[face:21]')
+
+            time_stamp = time.mktime(time_array)
+            if time.time() >= time_stamp:
+                return reply.text(
+                    '博士，过去的只能成为了过去，我们只需要朝着我们的未来前进就好，可以的话，阿米娅会一直陪在博士身边的[face:21]')
+
+            text = calc_result(time_stamp)
+
+            return reply.text(text)
+
+    except ValueError:
+        return reply.text('博士，这个日期真的没问题吗？')
+    except OverflowError:
+        return reply.text('博士，阿米娅算不过来了… >.<')
+
+
+def calc_result(end_date: float):
+    dates = calc_date(end_date)
+    end_date_str = stamp_to_date(end_date)
+
+    types = {'s': 0, 'd': 0, 't': 0, 'b': 0}
+
+    for item in dates:
+        types['s'] += sign_in
+        types['d'] += daily_tasks
+        if item['weekDate'] == 0:
+            types['t'] += weekly_tasks
+            types['b'] += weekly_battle
+
+    jade = 0
+    for i in types:
+        jade += types[i]
+
+    result = f'博士，不含制造站，罗德岛可在 {end_date_str} 前\n预计得到 {jade} 合成玉\n\n'
+
+    result += '- 月卡签到共计：%d\n' % types['s']
+    result += '- 每日任务共计：%d\n' % types['d']
+    result += '- 每周任务共计：%d\n' % types['t']
+    result += '- 剿灭行动共计：%d\n' % types['b']
+
+    result += '\n博士，要好好规划罗德岛的资源使用哦～'
+
+    return result
+
+
+def calc_date(end_date: float):
+    now_time = date_to_stamp(stamp_to_date(int(time.time())))
+
+    dates = []
+
+    while now_time < end_date:
+        now_time += 86400
+        time_array = time.localtime(now_time)
+        dates.append({
+            'dateStr': stamp_to_date(now_time),
+            'weekDate': time_array.tm_wday
+        })
+
+    return dates
+
+
+def date_to_stamp(date: str):
+    time_stamp = time.strptime(date, '%Y-%m-%d')
+    return int(time.mktime(time_stamp))
+
+
+def stamp_to_date(stamp: float):
+    time_array = time.localtime(stamp)
+    return time.strftime('%Y-%m-%d', time_array)
---
+++ src/arknights/calculator/money.py
@@ -0,0 +1,89 @@
+from typing import List, Dict, Tuple
+from dataclasses import dataclass
+
+# 招聘单价，第一个为 1小时0分钟，第二个为 1小时10分钟，以此类推到 9小时
+recruit_prices = [
+    140, 143, 147, 150, 154, 157,
+    161, 164, 168, 171, 175, 178,
+    182, 185, 189, 192, 196, 199,
+    203, 245, 252, 259, 266, 273,
+    280, 287, 294, 301, 308, 315,
+    322, 329, 336, 343, 350, 357,
+    364, 371, 378, 385, 490, 504,
+    518, 532, 546, 560, 574, 588,
+    602
+]
+
+
+# 小时分钟选几个 tag 等于多少钱
+@dataclass
+class Recruit:
+    hour: int
+    minute: int
+    tags: int
+    price: int
+
+
+def calc_money(money: int):
+    mutable_list: List[Recruit] = []
+    minute = 60
+    # 把每个不同时间的招聘单价都丢进去
+    for price in recruit_prices:
+        mutable_list.append(Recruit(minute // 60, minute % 60, 0, price))
+        minute += 10
+
+    mutable_list2: List[Recruit] = []
+    # 再把每个时间拼 1-3 个 tag 的招聘单价都丢进去
+    for recruit in mutable_list:
+        for tag in range(4):
+            mutable_list2.append(Recruit(recruit.hour, recruit.minute, tag, recruit.price + tag * 70))
+
+    # 得到了一个完整的招聘单价列表
+    # for item in mutable_list2:
+    #     print(f'公招{item.hour}小时{item.minute}分钟，选{item.tags}个tag需要{item.price}龙门币')
+
+    # 把招聘单价列表转成 map，方便后面查找
+    recruit_map: Dict[int, Recruit] = {item.price: item for item in mutable_list2}
+    array: List[int] = [n.price for n in sorted(mutable_list2, key=lambda x: x.price)]
+
+    # 初始化完成，下面是计算过程
+    # 凑硬币算法，参考 https://leetcode-cn.com/problems/coin-change/solution/322-ling-qian-dui-huan-by-leetcode-solution/
+    # 区别就是这里需要记录每个硬币的招聘配置
+    result: Tuple[int, List[int]] = coin_change(array, money, [0] * money, [[] for _ in range(money)])
+    if result[0] == -1:
+        return f'博士，无法凑出花费 {money} 龙门币的计划'
+    else:
+        if result[0] > 10:
+            return f'博士，花费这个数额的计划比较长，请分开计算吧'
+
+        text = f'博士，把 {money} 龙门币刚好花完，推荐 {result[0]} 次公招，分别是以下配置：\n\n'
+        for index in result[1]:
+            item = recruit_map[index]
+            text += f'{item.hour} 小时 {item.minute} 分钟，选 {item.tags} 个tag，花费 {item.price} 龙门币；\n'
+
+        return text
+
+
+def coin_change(coins: List[int], rem: int, count: List[int], money_list: List[List[int]]) -> Tuple[int, List[int]]:
+    if rem < 0:
+        return -1, []
+
+    if rem == 0:
+        return 0, []
+
+    if count[rem - 1] != 0:
+        return count[rem - 1], money_list[rem - 1]
+
+    min_val = float('inf')
+    min_list = []
+
+    for coin in coins:
+        res = coin_change(coins, rem - coin, count, money_list)
+        if 0 <= res[0] <= min_val:
+            min_val = 1 + res[0]
+            min_list = res[1] + [coin]
+
+    count[rem - 1] = -1 if min_val == float('inf') else min_val
+    money_list[rem - 1] = [] if min_val == float('inf') else min_list
+
+    return count[rem - 1], money_list[rem - 1]
