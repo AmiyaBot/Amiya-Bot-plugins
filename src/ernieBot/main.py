5865364467c80848ad3937a583e8de450e932ba0
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-11-27 10:13:27 +0800

    Update main.py

--- src/ernieBot/main.py
+++ src/ernieBot/main.py
@@ -12,7 +12,7 @@ from amiyabot.util import create_dir
 from amiyabot.log import LoggerManager
 from amiyabot import Message, Chain
 
-from core import AmiyaBotPluginInstance,Requirement
+from core import AmiyaBotPluginInstance, Requirement
 from core import bot as main_bot
 
 log = LoggerManager('ERNIEBot')
@@ -24,38 +24,38 @@ create_dir(resource_dir)
 if not os.path.exists(f'{resource_dir}/template'):
     shutil.copytree(f'{curr_dir}/template', f'{resource_dir}/template')
 
+
 def generate_schema():
+    filepath = f'{curr_dir}/config_schema.json'
 
-        filepath = f'{curr_dir}/config_schema.json'
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        log.info(f'Failed to load JSON from {filepath}.')
+        return None
 
-        try:
-            with open(filepath, 'r') as file:
-                data = json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            log.info(f"Failed to load JSON from {filepath}.")
-            return None
-        
-        model_list_str = ["..."]
+    model_list_str = ['...']
 
-        if 'amiyabot-blm-library' in main_bot.plugins:
-            blm_library = main_bot.plugins['amiyabot-blm-library']
-            model_list = blm_library.model_list()
+    if 'amiyabot-blm-library' in main_bot.plugins:
+        blm_library = main_bot.plugins['amiyabot-blm-library']
+        model_list = blm_library.model_list()
 
-            model_list_str = [model['model_name'] for model in model_list]
+        model_list_str = [model['model_name'] for model in model_list]
 
-        try:           
-            data["properties"]["model_name"]["enum"] = model_list_str
-        except KeyError as e:
-            stack_trace = traceback.format_exc()
-            log.info(f"Expected keys not found in the JSON structure: {e}\n{stack_trace}")
-        
-        return data
+    try:
+        data['properties']['model_name']['enum'] = model_list_str
+    except KeyError as e:
+        stack_trace = traceback.format_exc()
+        log.info(f'Expected keys not found in the JSON structure: {e}\n{stack_trace}')
+
+    return data
+
 
 class ERNIEBotPluginInstance(AmiyaBotPluginInstance):
     def get_template(self):
-
         setting = self.get_config('setting')
-        
+
         system_file = f'{resource_dir}/template/' + setting['system']
         template = None
 
@@ -78,9 +78,7 @@ bot = ERNIEBotPluginInstance(
     plugin_type='official',
     description='一个简易的使用BLM库进行聊天的插件',
     document=f'{curr_dir}/README.md',
-    requirements=[
-        Requirement("amiyabot-blm-library")
-    ],
+    requirements=[Requirement('amiyabot-blm-library')],
     instruction=f'{curr_dir}/README_USE.md',
     global_config_schema=generate_schema,
     global_config_default=f'{curr_dir}/config_default.yaml',
@@ -92,6 +90,7 @@ class ERNIEBotMessage:
     content: str
     role: str = 'user'
 
+
 @bot.on_message(keywords=re.compile('聊天$'))
 async def _(data: Message):
     # confirm = await data.wait(Chain(data).text('开启聊天模式？对话期间其他所有功能均不可用，回复【确认】开始聊天'))
@@ -99,7 +98,7 @@ async def _(data: Message):
     #     return
     if 'amiyabot-blm-library' not in main_bot.plugins:
         return Chain(data).text('未安装BLM库插件，无法使用聊天功能')
-    
+
     blm_library = main_bot.plugins['amiyabot-blm-library']
 
     model = bot.get_config('model_name')
@@ -116,7 +115,7 @@ async def _(data: Message):
     if not starting_prompt:
         return Chain(data).text('未找到人设模板文件')
 
-    res = await blm_library.chat_flow(f'{starting_prompt}请向大家简单介绍一下你自己。',model)
+    res = await blm_library.chat_flow(f'{starting_prompt}请向大家简单介绍一下你自己。', model)
     if not res:
         return
 
@@ -127,7 +126,7 @@ async def _(data: Message):
     while True:
         talk = await data.wait_channel(force=True, clean=False, max_time=60)
         if not talk:
-            res = await blm_library.chat_flow(f'{prompt}没人说话了，再见。',model)
+            res = await blm_library.chat_flow(f'{prompt}没人说话了，再见。', model)
             if res:
                 await data.send(Chain(last_msg, at=False).text(res))
             return
@@ -137,7 +136,7 @@ async def _(data: Message):
         messages = []
         for item in [last_msg] + talk.event.data:
             if item.text_original in ['不聊了', '结束']:
-                res = await blm_library.chat_flow(f'{prompt}聊天结束，再见。',model)
+                res = await blm_library.chat_flow(f'{prompt}聊天结束，再见。', model)
                 if res:
                     await data.send(Chain(item, at=False).text(res))
                 talk.close_event()
@@ -149,6 +148,6 @@ async def _(data: Message):
         talk.event.clean()
 
         if messages:
-            res = await blm_library.chat_flow(prompt + '\n\n'.join(messages),model)
+            res = await blm_library.chat_flow(prompt + '\n\n'.join(messages), model)
             if res:
                 await data.send(Chain(last_msg, at=False).text(res))
